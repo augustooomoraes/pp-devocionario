4418799c2d04fbcaebafbefcb086e623b1b6b97d
Augusto Moraes <cdm.augusto@gmail.com>
2024-07-13 16:05:40 -0300

    add theme toggle

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,22 @@
+"use client"
+ 
+import { useState, useEffect} from "react"
+import { useTheme } from "next-themes";
+import { Toggle } from "./ui/toggle";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Toggle onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      {theme === 'dark' ? 'Claro' : 'Escuro'}
+    </Toggle>
+  )
+}
