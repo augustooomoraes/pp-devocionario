919dd52eab27d619159ccde166ae7bc11326f3ef
Augusto Moraes <cdm.augusto@gmail.com>
2025-02-04 16:38:20 -0300

    fix production build errors

--- src/app/busca/page.tsx
+++ src/app/busca/page.tsx
@@ -1,9 +1,8 @@
 import { SearchResults } from "@/components/page-specific/buscar/searchResults";
 import RegularMain from "@/components/common/regularMain";
 
-export default function SearchPage({ searchParams }: { searchParams: { entrada?: string } }) {
+export default function Page({ searchParams }: { searchParams: { entrada?: string } }) {
   return (
-    // <></>
     <RegularMain>
       <SearchResults query={searchParams.entrada || ""} />
     </RegularMain>
