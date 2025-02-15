b12b0e41ba7f3ff4e884b1efedf7e7c4b861d4be
Augusto Moraes <cdm.augusto@gmail.com>
2025-02-15 15:27:37 -0300

    fix topbar covering page content - couldn't select anything below it

--- src/components/common/sidebar/topbar.tsx
+++ src/components/common/sidebar/topbar.tsx
@@ -9,10 +9,16 @@ export default function TopBar({
 }>) {
   return (
   <SidebarInset>
-    <header className="flex justify-start sticky top-0 bg-transparent h-14 shrink-0 items-center px-4">
+    <header className={
+        "flex justify-start items-center "
+      + "sticky top-0 h-14 shrink-0 bg-transparent "
+      // + "px-4 "
+      + "pl-4 "
+      + "w-min "
+    }>
       <div className="flex justify-start items-center">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
+        {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
       </div>
     </header>
     {children}
